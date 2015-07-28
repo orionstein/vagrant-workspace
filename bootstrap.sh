2b00b1ffebe6a188e34e8a76e9464c5b564d437b
Orion Free <ofree@upperquadrant.com>
2015-07-28 11:07:18 -0400

    Updating installation for working deps

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 sudo su
+lvresize -l +100%FREE /dev/vagrant-vg/root
+resize2fs /dev/vagrant-vg/root
 apt-get update
 apt-get install putty-tools
 rm /home/vagrant/$1
@@ -18,14 +20,24 @@ chmod 600 /home/$1/.ssh/authorized_keys
 chown $1:$1 /home/$1/.ssh/authorized_keys
 echo "$1 ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 ln -s /projects /home/$1/projects
+chown -R $1:$1 /home/$1/projects
 echo "\nhv_vmbus \nhv_storvsc \nhv_blkvsc \nhv_netvsc \n" >> /etc/initramfs-tools/modules
 update-initramfs -u
 su - $1
 curl -sL https://deb.nodesource.com/setup | sudo bash -
-sudo apt-get install -y ruby zsh git vim nodejs build-essential cmake python python-dev libpython-all-dev docker exuberant-ctags silversearcher-ag
+sudo curl -SLO https://get.docker.com/builds/Linux/x86_64/docker-latest
+sudo mv docker-latest /bin/docker
+sudo chmod +x /bin/docker
+sudo ln -s /bin/docker usr/bin/docker
+sudo curl -SLO https://raw.githubusercontent.com/docker/docker/master/contrib/init/systemd/docker.service
+sudo mv docker.service /etc/systemd/system/docker.service
+sudo curl -SLO https://raw.githubusercontent.com/docker/docker/master/contrib/init/systemd/docker.socket
+sudo mv docker.service /etc/systemd/system/docker.socket
+sudo groupadd docker
+sudo usermod -a -G docker $1
+sudo apt-get install -y ruby zsh git vim nodejs build-essential cmake python python-dev libpython-all-dev closure-linter exuberant-ctags silversearcher-ag
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
-sudo docker pull node:0.12.4-onbuild
 sudo chsh -s /usr/bin/zsh $1
 sudo -H -u $1 /usr/bin/zsh -c "mkdir ~/bin; cd ~/bin; curl -sSL http://deis.io/deis-cli/install.sh | sh; sudo ln -fs ~/bin/deis /usr/local/bin/deis; cd ~/.ssh; cp /vagrant/keys/deis/* .; find . -iname '*.pub' -exec deis keys:add {} \;"
 sudo -H -u $1 /usr/bin/zsh -c "git clone https://github.com/orionstein/rc.git ~/.rc; cd ~/.rc/; /usr/bin/ruby install.rb;"
